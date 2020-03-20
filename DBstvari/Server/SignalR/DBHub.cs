65442b80d7c235ac3f64914f2ace0474d3862457
Dragan Pepic <d.pepic@hotmail.com>
2020-03-20 21:00:52 +0100

    Add project files.

---
+++ DBstvari/Server/SignalR/DBHub.cs
@@ -0,0 +1,20 @@
+﻿using DBstvari.Shared;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBstvari.Server.SignalR
+{
+	public class DBHub : Hub
+	{
+		public async Task PrimiPodatke(List<Osoba> listO)
+		{
+			foreach(Osoba o in listO)
+			{
+				Console.WriteLine(o);
+			}
+		}
+	}
+}
---
+++ DBstvari/Shared/Osoba.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBstvari.Shared
+{
+	public class Osoba
+	{
+		public int ID { get; set; }
+		public string Ime { get; set; }
+		public string Prezime { get; set; }
+
+		public Osoba() { }
+		public Osoba(string i, string p)
+		{
+			Ime = i;
+			Prezime = p;
+		}
+
+		public override string ToString()
+		{
+			return $"{Ime} {Prezime}";
+		}
+	}
+}
